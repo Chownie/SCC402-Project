50bf550c64e9e09ac5d3e3548d554be4c10cc86f
chownie <sunderphon@gmail.com>
2017-11-06 19:17:26 +0000

    Viewable object tag, hypercolor

--- Assets/LanguageObject.cs
+++ Assets/LanguageObject.cs
@@ -5,4 +5,24 @@ using UnityEngine.UI;
 
 public class LanguageObject : MonoBehaviour {
 	public string word = "";
+	public Text text;
+	public CanvasGroup canvas;
+	GameObject cam;
+
+	void Start() {
+		text.text = word;
+		cam = GameObject.Find("Main Camera");
+	}
+
+	void Update () {
+		canvas.transform.rotation = Quaternion.LookRotation(canvas.transform.position - cam.transform.position);
+	}
+
+	public void FadeIn() {
+		canvas.alpha = 1;
+	}
+
+	public void FadeOut() {
+		canvas.alpha = 0;
+	}
 }
