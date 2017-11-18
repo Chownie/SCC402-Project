35ae6b3e35afea6a4f898f034ffbff8ee20e1bde
chownie <sunderphon@gmail.com>
2017-11-18 20:38:10 +0000

    Removed garbage, cleaned up helper scripts

---
+++ Assets/Scripts/LanguageObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageObject : MonoBehaviour {
+	public string word = "";
+	public Text text;
+	public CanvasGroup canvas;
+	GameObject cam;
+
+	void Start() {
+		text.text = word;
+		cam = GameObject.Find("Main Camera");
+	}
+
+	void Update () {
+		canvas.transform.rotation = Quaternion.LookRotation(canvas.transform.position - cam.transform.position);
+	}
+
+	public void getInputs() {
+		text.text = "";
+		text.text += "0: " +Input.GetKey(KeyCode.JoystickButton0).ToString() + "\n";
+		text.text += "1: " +Input.GetKey(KeyCode.JoystickButton1).ToString() + "\n";
+		text.text += "2: " +Input.GetKey(KeyCode.JoystickButton2).ToString() + "\n";
+		text.text += "3: " +Input.GetKey(KeyCode.JoystickButton3).ToString() + "\n";
+		text.text += "4: " +Input.GetKey(KeyCode.JoystickButton4).ToString() + "\n";
+		text.text += "5: " +Input.GetKey(KeyCode.JoystickButton5).ToString() + "\n";
+		text.text += "Horizontal: " + Input.GetAxis("Horizontal") + "\n";
+		text.text += "Vertical: " + Input.GetAxis("Vertical");
+		FadeIn();
+	}
+
+	public void FadeIn() {
+		canvas.alpha = 1;
+	}
+
+	public void FadeOut() {
+		canvas.alpha = 0;
+	}
+}
---
+++ Assets/Scripts/VrMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VrMovement : MonoBehaviour {
+	// Update is called once per frame
+	public enum ControlMethod{Smooth, Blink};
+
+	public ControlMethod controlMethod;
+
+	public float Speed = 1.0f;
+	public Transform facing;
+
+	public Image blinkImage;
+
+	public Camera camera;
+	void SmoothControl() {
+		float distance = Input.GetAxis("Vertical"); 
+		if (distance > 0.3) {
+			transform.position += (facing.forward*Speed) * Time.deltaTime;
+		}
+		if (distance < -0.3) {
+			transform.position -= (facing.forward*Speed) * Time.deltaTime;
+		}
+		transform.position = new Vector3(transform.position.x, 6f, transform.position.z);
+	}
+
+	void BlinkControl() {
+		/*if(Input.GetKeyDown(KeyCode.JoystickButton3)) {
+			Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)) {
+				blinkImage.CrossFadeAlpha(1.0f, 0.25f, false);
+				transform.position = hit.point;
+				blinkImage.CrossFadeAlpha(0.0f, 0.25f, false);
+			}
+		}*/
+	}
+
+	void Start() {
+		camera = facing.gameObject.GetComponent<Camera>();
+	}
+
+	void Update () {
+		if(controlMethod == ControlMethod.Smooth) {
+			SmoothControl();
+		}
+		if(controlMethod == ControlMethod.Blink) {
+			BlinkControl();
+		}
+	}
+}
